e0672c0a365c2ad2ac0a48662fcadf050e696f3b
Nikola Petric <petricn671@gmail.com>
2024-07-05 12:11:56 +0200

    removed duplicate

--- backend/src/main/java/com/example/repository/MenuItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.back.entities.MenuItems;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuItemRepository extends JpaRepository<MenuItems, Integer> {
-}
--- backend/src/main/java/com/example/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.back.repository;
-
-import com.example.back.entities.Reviews;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<Reviews, Integer> {
-}
